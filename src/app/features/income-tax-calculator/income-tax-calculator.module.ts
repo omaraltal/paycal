22747edcb2a1f6e76ff13ebb120b0c5f59bb6af5
Omar Altal <oaltal@woolworths.com.au>
2023-03-15 19:18:33 +1100

    upgrading angular material to 15

--- src/app/features/income-tax-calculator/income-tax-calculator.module.ts
+++ src/app/features/income-tax-calculator/income-tax-calculator.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatLegacyCheckboxModule as MatCheckboxModule } from '@angular/material/legacy-checkbox';
 
 import { CommonModule } from '@angular/common';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatLegacyRadioModule as MatRadioModule } from '@angular/material/legacy-radio';
+import { MatLegacySelectModule as MatSelectModule } from '@angular/material/legacy-select';
+import { MatLegacySlideToggleModule as MatSlideToggleModule } from '@angular/material/legacy-slide-toggle';
 import { SharedModule } from '@pc/shared/shared.module';
 import { IncomeTaxCalculatorRoutingModule } from './income-tax-calculator-routing.module';
 import { IncomeTaxCalculatorComponent } from './income-tax-calculator.component';
